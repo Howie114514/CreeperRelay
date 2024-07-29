7e01f5cb6652d5dc2cc7607eef27119407d7d372
Howie114514 <qiuchenghao666@outlook.com>
2024-07-29 13:13:48 +0800

    Fixed bugs:   camera.js

--- camera.js
+++ camera.js
@@ -2,32 +2,48 @@ const { Connection } = require("bedrock-protocol");
 const { Vector3, Vector2 } = require("./math");
 
 module.exports.camera = {
-    /**
-     * @type {Connection}
-     */
-    connection: undefined,
-    /**
-     * @param {Vector3} pos 
-     * @param {Vector2} rotation 
-     */
-    move(pos, rotation) {
-        if (!this.connection) { return }
-        this.connection.queue("camera_instruction", {
-            instruction_set: {
-                runtime_id: 0,
-                position: pos,
-                rotation: rotation,
-                ease_data: {
-                    type: "Linear",
-                    duration: 0.08
-                }
-            }
+  presets: {},
+  /**
+   * @type {Connection}
+   */
+  connection: undefined,
+  set connection(connection) {
+    this.connection = connection;
+    this.connection.on("clientbound", ({ name, params }, dest) => {
+      if (name == "camera_presets") {
+        params.presets.forEach((p, i) => {
+          this.presets[p.name] = p;
+          this.presets[p.name].index = i;
         });
-    },
-    clear() {
-        if (!this.connection) { return }
-        this.connection.queue("camera_instruction", {
-            clear: true
-        });
+      }
+    });
+  },
+  /**
+   * @param {Vector3} pos
+   * @param {Vector2} rotation
+   */
+  move(pos, rotation) {
+    if (!this.connection) {
+      return;
     }
-}
+    this.connection.queue("camera_instruction", {
+      instruction_set: {
+        runtime_id: this.presets["minecraft:free"].index,
+        position: pos,
+        rotation: rotation,
+        ease_data: {
+          type: "Linear",
+          duration: 0.08,
+        },
+      },
+    });
+  },
+  clear() {
+    if (!this.connection) {
+      return;
+    }
+    this.connection.queue("camera_instruction", {
+      clear: true,
+    });
+  },
+};
